7347cd11abdc4d4dbd9b33490d5f80ecf15022cf
Maks Hrech <grechemax1@gmail.com>
2026-01-25 22:30:41 +0100

    Add beforeEach; Add firstAidKit test

--- cypress/e2e/checkout_test.cy.js
+++ cypress/e2e/checkout_test.cy.js
@@ -1,7 +1,8 @@
 import CheckoutPage from '../support/pages/CheckoutPage';
+import FirstAidKitPage from "../support/pages/FirstAidKitPage";
 import HomePage from "../support/pages/HomePage";
 import HelmetPage from "../support/pages/HelmetPage";
-import {generateRandomEmail, generateRandomName, generateRandomRussianPhone} from "../support/utils";
+import {generateRandomEmail, generateRandomName, generateRandomPhone} from "../support/utils";
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   if (err.message.includes('scrollHeight')) {
@@ -11,36 +12,63 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 });
 
 describe('e2e checkout tests', () => {
-    before('open page', () => {
-        cy.visit('/');
-    })
-    let phone = generateRandomRussianPhone()
-    let name = generateRandomName()
-    let email = generateRandomEmail(name)
+    let phone;
+    let name;
+    let email;
+  
+  
+  // let helmets;
+    // let selectedHelmet;
+
+    // before('open page and load fixtures', () => {
+    //     cy.visit('/');
+    //     cy.fixture('helmets').then((data) => {
+    //         helmets = data;
+
+    //         selectedHelmet = helmets[Math.floor(Math.random() * helmets.length)];
+    //         cy.log(`Selected helmet: ${selectedHelmet.name}`);
+    //     });
+    // })
+    
+    beforeEach('generate user data', () => {
+      phone = generateRandomPhone()
+      name = generateRandomName()
+      email = generateRandomEmail(name)
+    });
 
     it('Buy a helmet from best selling proposal', () => {
+        cy.visit('/');
         HomePage.openProductByName('Беркут');
         HelmetPage.selectOptions()
         HelmetPage.selectAmount()
         HelmetPage.addToBasket()
         HelmetPage.clickCheckout()
-        CheckoutPage.clickCheckout()
-        cy.closePromoIfPresent();
 
+        CheckoutPage.placeOrder()
         CheckoutPage.enterPersonalDetails(name, phone, email);
         CheckoutPage.acceptTerms();
-        CheckoutPage.placeOrder()
-
+        CheckoutPage.clickConfirmOrder();
         CheckoutPage.verifyOrderSuccess();
         
-        CheckoutPage.getOrderDetails().then((orderNumber) => {
-            cy.logOrderNumber(orderNumber);
+        CheckoutPage.getOrderDetails().then((details) => {
+            cy.logOrderDetails(details);
         });
     });
 
-    // it('Buy a X from Y', () => {
-    //     cy.scrollTo('top');
-    //     HomePage.hoverOverCatalog();
-    //     HomePage.clickOnSubcategory('Аптечки и подсумки ');
-    // });
+    it('Buy a first Aid kit', () => {
+        cy.visit('/product-category/katalog/aptechki');
+        FirstAidKitPage.openProductByName('Мох');
+        FirstAidKitPage.addToBasket();
+        FirstAidKitPage.clickCheckout();
+        
+        CheckoutPage.placeOrder()
+        CheckoutPage.enterPersonalDetails(name, phone, email);
+        CheckoutPage.acceptTerms();
+        CheckoutPage.clickConfirmOrder();
+        CheckoutPage.verifyOrderSuccess();
+        
+        CheckoutPage.getOrderDetails().then((details) => {
+            cy.logOrderDetails(details);
+        });
+    });
 })
