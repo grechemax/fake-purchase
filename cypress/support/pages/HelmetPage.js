0fc5ad9d22874407df93644f3da84b04eea6ec35
Maks Hrech <grechemax1@gmail.com>
2026-01-28 16:57:00 +0100

    Small issues

--- cypress/support/pages/HelmetPage.js
+++ cypress/support/pages/HelmetPage.js
@@ -6,29 +6,30 @@ class HelmetPage extends BasePage {
      * Choose required product options.
      */
     selectOptions() {
-        // Material
+        // Material (2 options available)
+        let random = Math.floor(Math.random() * 2) + 1;
         cy.get('#material-izgotovleniya').select(1);
 
-        // Helmet cover
-        cy.get(
-            ':nth-child(8) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box'
-        ).click();
+        // // Helmet cover (8 options available)
+        // cy.get(
+        //     ':nth-child(8) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box'
+        // ).click();
 
-        // Balaclava
-        let random = Math.floor(Math.random() * 5) + 1;
-        cy.get(
-            `.billing_2 > .value > :nth-child(${random}) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box`
-        ).click();
+        // // Balaclava (5 options available)
+        // // let random = Math.floor(Math.random() * 5) + 1;
+        // cy.get(
+        //     `.billing_2 > .value > :nth-child(${random}) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box`
+        // ).click();
 
-        // Goggles
-        cy.get(
-            '.billing_3 > .value > :nth-child(4) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box'
-        ).click();
+        // // Goggles (4 options available)
+        // cy.get(
+        //     '.billing_3 > .value > :nth-child(4) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box'
+        // ).click();
 
-        // Headphones and mounts
-        cy.get(
-            '.billing_4 > .value > :nth-child(1) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box'
-        ).click();
+        // // Headphones and mounts (2 options available)
+        // cy.get(
+        //     '.billing_4 > .value > :nth-child(1) > .thwepo-image-group-lable > .thwepo-img-wrap > .thwepo-image-box'
+        // ).click();
 
     }
 
